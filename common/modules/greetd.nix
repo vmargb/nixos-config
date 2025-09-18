fbc2a662911e3818fc0275ba91854b9eb1791e78
vmargb <sharificles@gmail.com>
2025-09-18 19:13:02 +0100

    added sessions, theming to greeter

--- common/modules/greetd.nix
+++ common/modules/greetd.nix
@@ -4,14 +4,35 @@
   services.greetd = {
     enable = true;
     settings = {
-      default_session.command = ''
-        ${pkgs.greetd.tuigreet}/bin/tuigreet \
-          --time \
-          --asterisks \
-          --user-menu \
-          --cmd niri
-      '';
+      default_session = {
+        command = ''
+          ${pkgs.greetd.tuigreet}/bin/tuigreet \
+            --time \
+            --asterisks \
+            --user-menu \
+            --sessions
+        '';
+        user = "greeter";
+      };
+
+      sessions = [
+        {
+          name = "niri";
+          command = "${pkgs.niri}/bin/niri";
+        }
+        {
+          name = "sway";
+          command = "${pkgs.sway}/bin/sway";
+        }
+        {
+          name = "kde";
+          command = "${pkgs.plasma-workspace}/bin/startplasma-wayland";
+        }
+      ];
     };
   };
+
+  # stylix theming for tuigreet
+  stylix.targets.greetd.enable = true;
 }
 
